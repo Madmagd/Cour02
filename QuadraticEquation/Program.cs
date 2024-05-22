5b8c8f043003fed025f8478c5f6e8b59934938d0
Madm <frenk9219@gmail.com>
2024-05-22 19:58:30 +0200

    Done project  QuadraticEquation

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -1,3 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть значення a, b, c для рівняння ax^2 + bx + c = 0");
+        Console.Write("Значення a: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("Значення b: ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("Значення c: ");
+        double c = double.Parse(Console.ReadLine());
+        Console.WriteLine($"\n{a}x^2 + {b}x + {c} = 0");
 
+        double d = Math.Pow(b, 2) - 4 * a * c;
+        switch (d)
+        {
+            case > 0:
+                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine($"Значення x1 = {x1}\nЗначення x2 = {x2}");
+                break;
+            case 0:
+                double x = -b / (2 * a);
+                Console.WriteLine($"Значення x = {x}");
+                break;
+            case < 0:
+                Console.WriteLine("Рівняння не має розв'язку");
+                break;
+        }
+    }
+}
