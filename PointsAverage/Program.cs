f4f2841db67a68600c94f5c1fb6f2c0c55848109
Madm <frenk9219@gmail.com>
2024-05-22 20:40:14 +0200

    Done project PointsAverage

---
+++ PointsAverage/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть оцінки студента за 100 бальною шкалою:");
+        float grade = 0;
+        float sumOfGrades = 0;
+        for (int i = 0; i < 5; i++)
+        {
+            do
+            {
+                Console.Write($"Оцінка №{i + 1}: ");
+                grade = float.Parse(Console.ReadLine());
+            } while (grade < 0 || grade > 100);
+            sumOfGrades += grade;
+
+        }
+        sumOfGrades = sumOfGrades / 5;
+        if (sumOfGrades < 50)
+        {
+            Console.WriteLine($"\nСередня оцінка студента: {sumOfGrades}\nСтудент не допущений до екзамену");
+        }
+        else
+        {
+            Console.WriteLine($"\nСередня оцінка студента: {sumOfGrades}\nСтудент допущений до екзамену");
+        }
+
+    }
+}
