aac5340a3b63badebb5766c231578114ad975f9c
Madm <frenk9219@gmail.com>
2024-05-30 18:08:32 +0200

    Updated project QuadraticEquation

--- QuadraticEquation/Program.cs
+++ QuadraticEquation/Program.cs
@@ -12,20 +12,27 @@
         Console.WriteLine($"\n{a}x^2 + {b}x + {c} = 0");
 
         double d = Math.Pow(b, 2) - 4 * a * c;
-        switch (d)
+
+        if (a == 0)
+            Console.WriteLine("Вираз не є квадратним рівнянням");
+        else
         {
-            case > 0:
-                double x1 = (-b + Math.Sqrt(d)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(d)) / (2 * a);
-                Console.WriteLine($"Значення x1 = {x1}\nЗначення x2 = {x2}");
-                break;
-            case 0:
-                double x = -b / (2 * a);
-                Console.WriteLine($"Значення x = {x}");
-                break;
-            case < 0:
-                Console.WriteLine("Рівняння не має розв'язку");
-                break;
+            switch (d)
+            {
+                case > 0:
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    double x2 = (-b - Math.Sqrt(d)) / (2 * a);
+                    Console.WriteLine($"Значення x1 = {x1}\nЗначення x2 = {x2}");
+                    break;
+                case 0:
+                    double x = -b / (2 * a);
+                    Console.WriteLine($"Значення x = {x}");
+                    break;
+                case < 0:
+                    Console.WriteLine("Рівняння не має розв'язку");
+                    break;
+            }
         }
+        
     }
 }
